f78f9d391db8f9e0573e55ceca68daf20b46c8cc
joekendal <13680617+joekendal@users.noreply.github.com>
2021-11-27 13:44:54 +0000

    chore: projen settings

--- .projenrc.js
+++ .projenrc.js
@@ -1,18 +1,23 @@
-const { AwsCdkConstructLibrary } = require('projen');
+const { AwsCdkConstructLibrary, NpmAccess } = require('projen');
 const project = new AwsCdkConstructLibrary({
-  author: 'joekendal',
-  authorAddress: '13680617+joekendal@users.noreply.github.com',
+  author: 'nexusmed',
+  authorAddress: 'open-source@nexusmed.io',
   cdkVersion: '1.95.2',
   defaultReleaseBranch: 'main',
   name: 'aws-keystone',
-  repositoryUrl: 'https://github.com/13680617+joekendal/aws-keystone.git',
-
+  repositoryUrl: 'https://github.com/nexusmed/aws-keystone.git',
+  prerelease: 'alpha',
+  codeCov: true,
+  license: "MIT",
+  devContainer: true,
+  keywords: ['cdk','keystone','aws'],
+  packageName: 'aws-keystone',
+  description: 'Serverless deployment of Keystone on AWS',
+  npmAccess: NpmAccess.PUBLIC,
   // cdkDependencies: undefined,      /* Which AWS CDK modules (those that start with "@aws-cdk/") does this library require when consumed? */
   // cdkTestDependencies: undefined,  /* AWS CDK modules required for testing. */
   // deps: [],                        /* Runtime dependencies of this module. */
-  // description: undefined,          /* The description is just a string that helps people understand the purpose of the package. */
   // devDeps: [],                     /* Build dependencies for this module. */
-  // packageName: undefined,          /* The "name" in package.json. */
   // release: undefined,              /* Add release management to this project. */
 });
 project.synth();
